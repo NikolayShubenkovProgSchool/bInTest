6288aeb75470afdd1084e79e49ac4652047edb72
Naithar <devias.naith@gmail.com>
2016-05-31 10:09:08 +0300

    initial commit

---
+++ bInTest/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  bInTest
+//
+//  Created by Sergey Minakov on 31.05.16.
+//  Copyright © 2016 Naithar. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
